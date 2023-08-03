412443a1a7173fb3e4747e6865c5422ca5d7d829
peimanhp <peiman_hp@yahoo.com>
2023-08-03 13:33:13 +0330

    movie slider breakpoints updated

--- src/components/MovieSlider.jsx
+++ src/components/MovieSlider.jsx
@@ -13,18 +13,18 @@ const responsive = {
   },
   desktop: {
     breakpoint: { max: 992, min: 783 },
-    items: 7,
-    slidesToSlide: 7,
+    items: 5,
+    slidesToSlide: 5,
   },
   tablet: {
-    breakpoint: { max: 783, min: 483 },
-    items: 4,
-    slidesToSlide: 4,
+    breakpoint: { max: 783, min: 390 },
+    items: 2,
+    slidesToSlide: 2,
   },
   mobile: {
-    breakpoint: { max: 483, min: 0 },
-    items: 3,
-    slidesToSlide: 3,
+    breakpoint: { max: 390, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
   },
 };
 
